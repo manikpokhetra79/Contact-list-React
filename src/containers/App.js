d157151be076931632fc365c6e93536aa7772964
Manik Gupta <manikpokhetra@gmail.com>
2021-09-29 23:04:47 +0530

    added react-router and react bootstrap classes for styling

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import ContactList from '../components/ContactList';
 import ContactForm from '../components/ContactForm';
 import { userApi } from '../helpers/Urls';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -23,10 +25,53 @@ class App extends React.Component {
   render() {
     const { users } = this.state;
     return (
-      <div>
-        <h1>Contact Manager</h1> <ContactForm />
-        <ContactList users={users} />
-      </div>
+      <Router>
+        {' '}
+        <div>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand>
+                <Link to="/" className="text-light text-decoration-none">
+                  Contact Manager
+                </Link>
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link>
+                    <Link
+                      to="/contacts"
+                      className="text-light text-decoration-none"
+                    >
+                      Contact List
+                    </Link>
+                  </Nav.Link>
+                  <Nav.Link>
+                    <Link
+                      to="/add-contacts"
+                      className="text-light text-decoration-none"
+                    >
+                      New Contact
+                    </Link>
+                  </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+          <Switch>
+            <Route exact path="/">
+              <h1>Hi, this is a contact manager app</h1>
+            </Route>
+            <Route path="/contacts">
+              <ContactList users={users} />
+            </Route>
+            <Route path="/add-contacts">
+              <h1>Contact Manager</h1>
+              <ContactForm />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
